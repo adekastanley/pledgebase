2073ece2729ee18336c09b66314e71d8d7bf6916
adeka stanley <adekastanley2@gmail.com>
2024-10-04 13:07:45 +0100

    cache clean

--- src/components/navigation/Mobile.tsx
+++ src/components/navigation/Mobile.tsx
@@ -1,4 +1,4 @@
-import React, { useRef, useState, useEffect, MutableRefObject } from "react";
+import React, { useRef, useState, useEffect } from "react";
 import gsap from "gsap";
 import { FaBars } from "react-icons/fa6";
 import { FaTimes } from "react-icons/fa";
