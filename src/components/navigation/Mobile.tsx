0f8c7704b98ff75c915a10cf6ff1cb9f84d83f9e
adeka stanley <adekastanley2@gmail.com>
2024-10-06 10:34:32 +0100

    mobile test upload for wallet

--- src/components/navigation/Mobile.tsx
+++ src/components/navigation/Mobile.tsx
@@ -3,7 +3,19 @@ import gsap from "gsap";
 import { FaBars } from "react-icons/fa6";
 import { FaTimes } from "react-icons/fa";
 import { Button } from "../ui/button";
-import { WalletComponents } from "../user/WalletComponent";
+// import { WalletComponents } from "../user/WalletComponent";
+
+// wallet
+import {
+	ConnectWallet,
+	Wallet,
+	WalletDropdown,
+	WalletDropdownDisconnect,
+} from "@coinbase/onchainkit/wallet";
+import { Address, Avatar, Name, Identity } from "@coinbase/onchainkit/identity";
+import { color } from "@coinbase/onchainkit/theme";
+
+import { Dialog, DialogContent, DialogTrigger } from "@/components/ui/dialog";
 
 // Define the type for each menu item
 type MenuLink = {
@@ -42,7 +54,8 @@ const menuLinks: MenuLink[] = [
 ];
 
 const Mobile: React.FC = () => {
-	// Refs with proper typing for elements that are initially `null`
+	const [open, setOpen] = React.useState(false);
+
 	const container = useRef<HTMLElement | null>(null);
 	const overlayRef = useRef<HTMLDivElement | null>(null);
 	const menuItemsRef = useRef<(HTMLLIElement | null)[]>([]);
@@ -103,9 +116,17 @@ const Mobile: React.FC = () => {
 			<div className="w-full flex items-center justify-between py-4 px-6  fixed z-[20]  ">
 				<div className="text-black font-bold text-lg">LOGO</div>
 				<div className="flex items-center gap-2">
-					<Button size={"sm"}>
+					{/* <Button size={"sm"}>
 						<WalletComponents />
-					</Button>
+					</Button> */}
+					<Dialog open={open} onOpenChange={setOpen}>
+						<DialogTrigger asChild>
+							<Button variant="outline"> Wallet</Button>
+						</DialogTrigger>
+						<DialogContent className="w-auto">
+							<WalletComponents />
+						</DialogContent>
+					</Dialog>
 					<button onClick={toggleNav} className="text-black">
 						{isMenuOpen ? <FaTimes size={30} /> : <FaBars size={30} />}
 					</button>
@@ -138,3 +159,25 @@ const Mobile: React.FC = () => {
 };
 
 export default Mobile;
+
+export function WalletComponents() {
+	return (
+		<div className="flex justify-end ">
+			<Wallet className="">
+				<ConnectWallet>
+					<Avatar className="h-6 w-6" />
+					<Name />
+				</ConnectWallet>
+				<WalletDropdown className="bg-black">
+					<Identity className="px-4 pt-3 pb-2 " hasCopyAddressOnClick>
+						<Avatar className="shadow-sm mx-1 border-2 border-primary " />
+						<Name className=" my-1" />
+						<Address className={""} />
+						<Address className={color.foregroundMuted} />
+					</Identity>
+					<WalletDropdownDisconnect className="bg-red-500 text-white  text-center w-full pl-10 pb-2 rounded  " />
+				</WalletDropdown>
+			</Wallet>
+		</div>
+	);
+}
