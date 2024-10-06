b37f078080c44d9a7e0639c3059966c5b5ef0c6c
adeka stanley <adekastanley2@gmail.com>
2024-10-06 21:06:37 +0100

    new mobile

--- src/components/screens/Hero.tsx
+++ src/components/screens/Hero.tsx
@@ -2,7 +2,7 @@
 import { BsChevronDoubleDown } from "react-icons/bs";
 const Hero = () => {
 	return (
-		<div className="min-h-[100vh] flex justify-center  relative bg-[url('src/assets/hands.svg')] bg-cover">
+		<div className="min-h-[100vh] flex justify-center  relative bg-[url('src/assets/hands.svg')] bg-cover bg-left-bottom">
 			<div className="mt-[10rem] lg:mt-[14rem] text-center">
 				<h1 className="font-bold text-5xl sm:text-7xl lg:text-9xl uppercase ">
 					{/* PledgeBase */}
