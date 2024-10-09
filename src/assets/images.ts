2993f942a523f6698d85506c1f1b093e48963945
adeka stanley <adekastanley2@gmail.com>
2024-10-09 20:03:08 +0100

    progress

--- src/assets/images.ts
+++ src/assets/images.ts
@@ -1,10 +1,16 @@
 import school1 from "./images/school-1.jpeg";
 import kids1 from "./images/kids1.jpg";
+
 import girl1 from "./images/girl1.jpg";
 import girl2 from "./images/girl2.jpeg";
 import girl3 from "./images/girl3.jpeg";
 
+import hero from "./kids.jpg";
+
 export const schoolsImages = [school1, kids1];
 export const girls = [girl2, girl3, girl1];
+export const images = {
+	hero: hero,
+};
 
 export default schoolsImages;
--- src/components/screens/Hero.tsx
+++ src/components/screens/Hero.tsx
@@ -1,16 +1,22 @@
 // "Global Causes, Local Impact"
 import { BsChevronDoubleDown } from "react-icons/bs";
+import { images } from "@/assets/images";
+
 const Hero = () => {
 	return (
-		<div className="min-h-[100vh] bg-white flex justify-center  relative bg-[url('src/assets/hands.svg')] bg-cover bg-left-bottom ">
-			<div className="mt-[10rem] lg:mt-[14rem] text-center">
-				<h1 className="font-bold text-5xl sm:text-7xl lg:text-9xl uppercase ">
-					{/* PledgeBase */}
-					CrowdBase
-				</h1>
-				<h2 className="font-semibold text-2xl">
-					Building a Better Tomorrow Together
-				</h2>
+		<div className="min-h-[80vh] bg-white flex  flex-col items-center relative bg-[] bg-cover bg-left-bottom ">
+			<div className="   flex flex-col sm:flex-row w-full  items-center sm:px-20 pt-20 ">
+				<div className="w-full flex flex-col justify-center sm:items-center px-1 h-full ">
+					<h1 className="font-bold uppercase text-5xl lg:text-9xl">
+						CrowdBase
+					</h1>
+					<h2 className="font-semibold text-xl pl-2 sm:pl-4 text-start w-full ">
+						Building a Better Tomorrow Together
+					</h2>
+				</div>
+				<div className="w-full h-full flex items-center">
+					<img src={images.hero} className="w-full" alt="" />
+				</div>
 			</div>
 			<div className="absolute bottom-20 ">
 				<BsChevronDoubleDown size={20} className="" />
