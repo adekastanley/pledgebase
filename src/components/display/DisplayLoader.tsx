9c3667a446e9496b3966b08857039be538b3fbea
adeka stanley <adekastanley2@gmail.com>
2024-10-09 12:05:55 +0100

    progress

--- src/components/display/DisplayLoader.tsx
+++ src/components/display/DisplayLoader.tsx
@@ -2,7 +2,7 @@
 import gsap from "gsap";
 import { useGSAP } from "@gsap/react";
 import { useRef } from "react";
-import { girls } from "@/assets/images";
+
 //@ts-ignore
 const DisplayLoader = (props) => {
 	gsap.registerPlugin(useGSAP);
